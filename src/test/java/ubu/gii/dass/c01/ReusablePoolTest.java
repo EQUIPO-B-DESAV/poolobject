174a1da7ca1b92bd688ff10c7726acd884b7f0e9
Lalucira <lma@lalucira.com>
2026-02-20 16:37:26 +0100

    testGetInstance implementado

--- src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
+++ src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
@@ -3,7 +3,11 @@
  */
 package ubu.gii.dass.c01;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -14,7 +18,8 @@ import org.junit.jupiter.api.Disabled;
 
 
 /**
- * @author alumno
+ * Clase de pruebas de código.
+ * @author Lucía Martín Asenjo
  *
  */
 public class ReusablePoolTest {
@@ -22,6 +27,7 @@ public class ReusablePoolTest {
 	
 	@BeforeAll
 	public static void setUp(){
+		
 	}
 
 	
@@ -31,12 +37,20 @@ public class ReusablePoolTest {
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#getInstance()}.
+	 * @author Lucía Martín Asenjo
 	 */
-        @Test
+     @Test
         @DisplayName("testGetInstance")
-        @Disabled("Not implemented yet")
 	public void testGetInstance() {
-		
+        	try {
+                ReusablePool pool1 = ReusablePool.getInstance();
+                ReusablePool pool2 = ReusablePool.getInstance();
+                assertNotNull(pool1, "La instancia pool1 no debe ser nula.");
+                assertNotNull(pool2, "La instancia pool2 no debe ser nula.");
+                assertSame(pool1, pool2, "Ambas instancias deben referir al mismo objeto.");
+            } catch (Exception e) {
+                fail("testGetInstance: " + e.getMessage());
+            }
 	}
 
 	/**
